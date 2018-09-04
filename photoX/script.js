89d3d1c3ba3106d836a94c384b6d045fe8343b23
Dennis Xiao <dennisboys@gmail.com>
2018-09-04 22:16:23 +1000

    add navbar

--- photoX/script.js
+++ photoX/script.js
@@ -1,32 +1,18 @@
+$(document).ready(() => {
 
+  // animate the navbar close button
+  $('.nav-button').click(() => {
+    $('.nav-button').toggleClass('change');
+  })
 
+  $(window).scroll(() => {
+    let position = $(this).scrollTop();
 
+    if (position >= 200) {
+      $('.nav-menu').addClass('custom-navbar');
+    } else {
+      $('.nav-menu').removeClass('custom-navbar');
+    }
+  })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
