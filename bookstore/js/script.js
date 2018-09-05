bc4ed28146d77fd907a95dbab5bfdefcc2ded7ce
Dennis Xiao <dennisboys@gmail.com>
2018-09-05 20:42:26 +1000

    add famous writer section

--- bookstore/js/script.js
+++ bookstore/js/script.js
@@ -1,12 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // animate the gallery section when user scrolls to a certain position
-  $(window).scroll(function() {
-    let position = $(this).scrollTop();
+  $(window).scroll(function () {
+    let position = $(this).scrollTop()
 
-    if(position >= 350) {
-      $('.gallery').addClass('change');
+    if (position >= 350) {
+      $('.gallery').addClass('change')
     } else {
-      $('.gallery').removeClass('change');
+      $('.gallery').removeClass('change')
     }
-  });
-});
+  })
+
+  $('.writers-accordion').click(function (event) {
+    const clickTarget = event.target.id.split('-')[0];
+    const authorName = event.target.id.split('-')[1];
+
+    // check if user has clicked on a button
+    if (clickTarget === 'button') {
+      $('#book-1').attr('src', `img/writers/${authorName}-book1.jpg`)
+      $('#book-2').attr('src', `img/writers/${authorName}-book2.jpg`)
+    }
+  })
+})
