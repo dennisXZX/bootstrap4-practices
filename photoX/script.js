0d1be49d9d383e55d167e0d6de84688f60abaa63
Dennis Xiao <dennisboys@gmail.com>
2018-09-05 13:56:48 +1000

    add price section

--- photoX/script.js
+++ photoX/script.js
@@ -45,4 +45,19 @@ $(document).ready(function() {
   $('.gallery-list-item').click(function () {
     $(this).addClass('active-item').siblings().removeClass('active-item');
   })
+
+  // animate the price section when user scrolls to a certain position
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+
+    if (position >= 4300) {
+      $('.card-1').addClass('moveFromLeft');
+      $('.card-2').addClass('moveFromBottom');
+      $('.card-3').addClass('moveFromRight');
+    } else {
+      $('.card-1').removeClass('moveFromLeft');
+      $('.card-2').removeClass('moveFromBottom');
+      $('.card-3').removeClass('moveFromRight');
+    }
+  })
 });
