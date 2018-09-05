dba387d5cbd73f706c65694cd6d91d2d0c8c456d
Dennis Xiao <dennisboys@gmail.com>
2018-09-05 12:10:15 +1000

    add mission section

--- photoX/script.js
+++ photoX/script.js
@@ -5,6 +5,7 @@ $(document).ready(() => {
     $('.nav-button').toggleClass('change');
   })
 
+  // animate the navbar when it scrolls to a certain position
   $(window).scroll(() => {
     let position = $(this).scrollTop();
 
@@ -12,6 +13,18 @@ $(document).ready(() => {
       $('.nav-menu').addClass('custom-navbar');
     } else {
       $('.nav-menu').removeClass('custom-navbar');
+    }
+  })
+
+  $(window).scroll(() => {
+    let position = $(this).scrollTop();
+
+    if (position >= 650) {
+      $('.camera-img').addClass('fromLeft');
+      $('.mission-text').addClass('fromRight');
+    } else {
+      $('.camera-img').removeClass('fromLeft');
+      $('.mission-text').removeClass('fromRight');
     }
   })
 
