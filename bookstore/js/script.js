be291696ab76beeb2b0cff86b136cd6f6381bc47
Dennis Xiao <dennisboys@gmail.com>
2018-09-05 19:45:16 +1000

    add gallery section

--- bookstore/js/script.js
+++ bookstore/js/script.js
@@ -1,34 +1,12 @@
+$(document).ready(function() {
+  // animate the gallery section when user scrolls to a certain position
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if(position >= 350) {
+      $('.gallery').addClass('change');
+    } else {
+      $('.gallery').removeClass('change');
+    }
+  });
+});
